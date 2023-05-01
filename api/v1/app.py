97e9f9963dd919c5a9088b34048e641a81d84650
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-05-01 15:36:39 +0200

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,6 +10,12 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 @app.teardown_appcontext
 def teardown(self):
     storage.close()
+    
+@app.errorhandler(404)
+def not_found(error):
+    """ Returns JSON response with 404 status """
+    return make_response(jsonify({"error": "Not found"}), 404)
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
