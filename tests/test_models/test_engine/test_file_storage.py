55634dd91840b5d51646d32aca2164fb4a2b66a8
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-05-01 12:57:09 +0200

    Update test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -73,6 +73,9 @@ test_file_storage.py'])
 class TestFileStorage(unittest.TestCase):
     """Test the FileStorage class"""
     @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def setUp(self):
+        self.storage = FileStorage()
+
     def test_all_returns_dict(self):
         """Test that all returns the FileStorage.__objects attr"""
         storage = FileStorage()
