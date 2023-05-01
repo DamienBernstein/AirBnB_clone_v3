6ef7af825f8bacdb96d26d585d36c192d06a1e4b
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2023-05-01 11:48:40 +0200

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -75,21 +75,20 @@ class DBStorage:
         """call remove() method on the private session attribute"""
         self.__session.remove()
 
-   
-    def get(self, cls, id):
+     def get(self, cls, id):
         """
-        Returns the object based on the class name and its ID, or None if not
-        found
+        Returns the object based on the class and its ID, or None if not found
         """
-        key = "{}.{}".format(cls.__name__, id)
-        return self.__objects.get(key)
+        objects = self.__session.query(cls).filter_by(id=id).first()
+        return objects
 
-    def get_count(self, cls=None):
+    def count(self, cls=None):
         """
-        Returns the number of objects in storage matching the given class name.
-        If no name is passed, returns the count of all objects in storage.
+        Returns the number of objects in storage matching the given class. If no class
+        is passed, returns the count of all objects in storage.
         """
         if cls is None:
-            return len(self.__objects)
+            return self.__session.query(func.count(User.id)).scalar()
         else:
-            return sum(1 for key in self.__objects.keys() if key.startswith(cls.__name__))
+            return self.__session.query(func.count(cls.id)).scalar()
+ 
